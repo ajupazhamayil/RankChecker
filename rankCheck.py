a13c43416cab1759cf2029cbc620ce0e4b404161
Jyothis <jyothisjagan@gmail.com>
2018-10-30 22:15:10 +0530

    initial commit

---
+++ rankCheck.py
@@ -0,0 +1,13 @@
+
+import urllib, sys, re
+
+def rankCheck(rsk):
+    xml = urllib.urlopen('http://data.alexa.com/data?cli=10&dat=s&url=%s'%site).read()
+    try: rank = int(re.search(r'<POPULARITY[^>]*TEXT="(\d+)"', xml).groups()[0])
+    except: rank = -1
+    print 'Your rank for %s is %d\n' % (site, rank)
+
+
+
+site = "nitk.ac.in"
+rankCheck(site)
